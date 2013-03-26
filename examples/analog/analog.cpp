ee70b8e5207f41868474f682f4e5cf3a5e3d4e18
Mike Hord <mike.hord@sparkfun.com>
2013-03-26 16:07:57 +0000

    Analog output code added. Analog.h added. Analog output doesn't work for all pins.

--- examples/analog/analog.cpp
+++ examples/analog/analog.cpp
@@ -1,31 +1,53 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include "analog.h"
 
-#define PWM_IF_PATH "/sys/class/leds/"
-#define PWM_IF_FILE "pwm"
-
-#define ADC_IF_PATH "/proc/"
-#define ADC_IF_FILE "adc"
-
-int adc[6];
-int pwm[6];
-char path[64];
+int adc[6];     // Array for file descriptors for the adc pins
+char path[64];  // Nice big buffer for constructing file paths
 
 int main(void)
 {
+  // For starters, let's create file descriptors for all the ADC pins. PWM is
+  //   handled differently.
 	for(int i = 0; i<= 5; i++)
 	{
 		memset(path, 0, sizeof(path));
 		sprintf(path, "%s%s%d", ADC_IF_PATH, ADC_IF_FILE , i);
 		adc[i] = open(path, O_RDONLY);
-		memset(path, 0, sizeof(path));
-    sprintf(path,  "%s%s%d", PWM_IF_PATH, PWM_IF_FILE , i);
-    pwm[i] = open(path, O_RDWR);
 	}
   
-  char buf[32];
-  read(adc[0], buf, sizeof(buf));
-  printf("%s", buf);
+  // Now, we'll blast all the PWM pins to zero. 
+  for(int j = 0; j<=5; j++)
+  {
+    analogWrite(j, 0);
+  }
+  
+  // Now we can go about the business of dimming some LEDs. 
+  for(int j = 0; j<=5; j++)
+  {
+    for(int i = 0; i<=255; i++)
+    {
+      analogWrite(j, i);
+      usleep(25000);
+    }
+    analogWrite(j, 0);
+  }
+}
+
+  // PWM pin access is handled by
+  //   using the system() command to invoke the command process to execute a
+  //   command. We assemble the command using sprintf()- the command takes
+  //   the form
+  //   echo <value> > /sys/class/leds/pwmX/brightness
+  //   where <value> should be replaced by an integer from 0-255 and X should
+  //   be replaced by an index value from 0-5
+void analogWrite(int pin, int value)
+{
+  memset(path, 0, sizeof(path));
+  sprintf(path, "echo %d > %s%s%d/%s", value, PWM_IF_PATH, PWM_IF_FILE, \
+    pin, PWM_IF);
+  system(path);
 }
---
+++ examples/analog/analog.h
@@ -0,0 +1,13 @@
+#ifndef __analog_h__
+#define __analog_h__
+
+#define PWM_IF_PATH "/sys/class/leds/"
+#define PWM_IF_FILE "pwm"
+#define PWM_IF "brightness"
+
+#define ADC_IF_PATH "/proc/"
+#define ADC_IF_FILE "adc"
+
+void analogWrite(int pin, int value);
+
+#endif
